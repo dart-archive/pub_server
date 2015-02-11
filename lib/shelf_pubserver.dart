e1b3ff315b90154f266204ebf7d5a8e573f993f3
Martin Kustermann <kustermann@google.com>
2015-02-11 15:54:54 +0100

    Initial import from pub-dartlang-dart repository
    R=sgjesse@google.com
    
    Review URL: https://chromereviews.googleplex.com/150117013

---
+++ lib/shelf_pubserver.dart
@@ -0,0 +1,401 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library pub_server.shelf_pubserver;
+
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:logging/logging.dart';
+import 'package:mime/mime.dart';
+import 'package:pub_semver/pub_semver.dart';
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:yaml/yaml.dart';
+
+import 'repository.dart';
+
+final Logger _logger = new Logger('pubserver.shelf_pubserver');
+
+// TODO: Error handling from [PackageRepo] class.
+// Distinguish between:
+//   - Unauthorized Error
+//   - Version Already Exists Error
+//   - Internal Server Error
+/// A shelf handler for serving a pub [PackageRepository].
+///
+/// The following API endpoints are provided by this shelf handler:
+///
+///   * Getting information about all versions of a package.
+///
+///         GET /api/packages/<package-name>
+///         [200 OK] [Content-Type: application/json]
+///         {
+///           "name" : "<package-name>",
+///           "latest" : { ...},
+///           "versions" : [
+///             {
+///               "version" : "<version>",
+///               "archive_url" : "<download-url tar.gz>",
+///               "pubspec" : {
+///                 "author" : ...,
+///                 "dependencies" : { ... },
+///                 ...
+///               },
+///           },
+///           ...
+///           ],
+///         }
+///         or
+///         [404 Not Found]
+///
+///   * Getting information about a specific (package, version) pair.
+///
+///         GET /api/packages/<package-name>/versions/<version-name>
+///         [200 OK] [Content-Type: application/json]
+///         {
+///           "version" : "<version>",
+///           "archive_url" : "<download-url tar.gz>",
+///           "pubspec" : {
+///             "author" : ...,
+///             "dependencies" : { ... },
+///             ...
+///           },
+///         }
+///         or
+///         [404 Not Found]
+///
+///   * Downloading package.
+///
+///         GET /api/packages/<package-name>/versions/<version-name>.tar.gz
+///         [200 OK] [Content-Type: octet-stream ??? FIXME ???]
+///         or
+///         [302 Found / Temporary Redirect]
+///         Location: <new-location>
+///         or
+///         [404 Not Found]
+///
+///   * Uploading
+///
+///         GET /api/packages/versions/new
+///         Headers:
+///           Authorization: Bearer <oauth2-token>
+///         [200 OK]
+///         {
+///           "fields" : {
+///               "a": "...",
+///               "b": "...",
+///               ...
+///           },
+///           "url" : "https://storage.googleapis.com"
+///         }
+///
+///         POST "https://storage.googleapis.com"
+///         Headers:
+///           a: ...
+///           b: ...
+///           ...
+///         <multipart> file package.tar.gz
+///         [302 Found / Temporary Redirect]
+///         Location: https://pub.dartlang.org/finishUploadUrl
+///
+///         GET https://pub.dartlang.org/finishUploadUrl
+///         [200 OK]
+///         {
+///           "success" : {
+///             "message": "Successfully uploaded package.",
+///           },
+///        }
+///
+/// It will use the pub [PackageRepository] given in the constructor to provide
+/// this HTTP endpoint.
+class ShelfPubServer {
+  static final RegExp _packageRegexp =
+      new RegExp(r'^/api/packages/([^/]+)$');
+
+  static final RegExp _versionRegexp =
+      new RegExp(r'^/api/packages/([^/]+)/versions/([^/]+)$');
+
+  static final RegExp _downloadRegexp =
+      new RegExp(r'^/packages/([^/]+)/versions/([^/]+)\.tar\.gz$');
+
+  static final RegExp _boundaryRegExp = new RegExp(r'^.*boundary="([^"]+)"$');
+
+
+  final PackageRepository repository;
+
+  ShelfPubServer(this.repository);
+
+
+  Future<shelf.Response> requestHandler(shelf.Request request) {
+    String path = request.requestedUri.path;
+    if (request.method == 'GET') {
+      var downloadMatch = _downloadRegexp.matchAsPrefix(path);
+      if (downloadMatch != null) {
+        var package = Uri.decodeComponent(downloadMatch.group(1));
+        var version = Uri.decodeComponent(downloadMatch.group(2));
+        return _download(request.requestedUri, package, version);
+      }
+
+      var packageMatch = _packageRegexp.matchAsPrefix(path);
+      if (packageMatch != null) {
+        var package = Uri.decodeComponent(packageMatch.group(1));
+        return _listVersions(request.requestedUri, package);
+      }
+
+      var versionMatch = _versionRegexp.matchAsPrefix(path);
+      if (versionMatch != null) {
+        var package = Uri.decodeComponent(versionMatch.group(1));
+        var version = Uri.decodeComponent(versionMatch.group(2));
+        return _showVersion(request.requestedUri, package, version);
+      }
+
+      if (path == '/api/packages/versions/new') {
+        if (!repository.supportsUpload) {
+          return new Future.value(new shelf.Response.notFound(null));
+        }
+
+        if (repository.supportsAsyncUpload) {
+          return _startUploadAsync(request.requestedUri);
+        } else {
+          return _startUploadSimple(request.requestedUri);
+        }
+      }
+
+      if (path == '/api/packages/versions/newUploadFinish') {
+        if (!repository.supportsUpload) {
+          return new Future.value(new shelf.Response.notFound(null));
+        }
+
+        if (repository.supportsAsyncUpload) {
+          return _finishUploadAsync(request.requestedUri);
+        } else {
+          return _finishUploadSimple(request.requestedUri);
+        }
+      }
+    } else if (request.method == 'POST') {
+      if (!repository.supportsUpload) {
+        return new Future.value(new shelf.Response.notFound(null));
+      }
+
+      if (path == '/api/packages/versions/newUpload') {
+        return _uploadSimple(
+            request.requestedUri,
+            request.headers['content-type'],
+            request.read());
+      }
+    }
+    return new Future.value(new shelf.Response.notFound(null));
+  }
+
+
+  // Metadata handlers.
+
+  Future<shelf.Response> _listVersions(Uri uri, String package) {
+    return repository.versions(package).toList()
+        .then((List<PackageVersion> packageVersions) {
+      if (packageVersions.length == 0) {
+        return new shelf.Response.notFound(null);
+      }
+
+      packageVersions.sort((a, b) => a.version.compareTo(b.version));
+
+      // TODO: Add legacy entries (if necessary), such as version_url.
+      Map packageVersion2Json(PackageVersion version) {
+        return {
+          'archive_url':
+              '${_downloadUrl(
+                  uri, version.packageName, version.versionString)}',
+          'pubspec': loadYaml(version.pubspecYaml),
+          'version': version.versionString,
+        };
+      }
+
+      var latestVersion = packageVersions.last;
+      for (int i = packageVersions.length - 1; i >= 0; i--) {
+        if (!packageVersions[i].version.isPreRelease) {
+          latestVersion = packageVersions[i];
+          break;
+        }
+      }
+
+      // TODO: The 'latest' is something we should get rid of, since it's
+      // duplicated in 'versions'.
+      return _jsonResponse({
+        'name' : package,
+        'latest' : packageVersion2Json(latestVersion),
+        'versions' : packageVersions.map(packageVersion2Json).toList(),
+      });
+    });
+  }
+
+  Future<shelf.Response> _showVersion(Uri uri, String package, String version) {
+    return repository
+        .lookupVersion(package, version).then((PackageVersion version) {
+          if (version == null) {
+            return new shelf.Response.notFound('');
+          }
+
+          // TODO: Add legacy entries (if necessary), such as version_url.
+          return _jsonResponse({
+            'archive_url':
+                '${_downloadUrl(
+                    uri, version.packageName, version.versionString)}',
+            'pubspec': loadYaml(version.pubspecYaml),
+            'version': version.versionString,
+          });
+    });
+  }
+
+
+  // Download handlers.
+
+  Future<shelf.Response> _download(Uri uri, String package, String version) {
+    if (repository.supportsDownloadUrl) {
+      return repository.downloadUrl(package, version).then((Uri url) {
+        // This is a redirect to [url]
+        return new shelf.Response.seeOther(url);
+      });
+    }
+    return repository.download(package, version).then((stream) {
+      return new shelf.Response.ok(stream);
+    });
+  }
+
+
+  // Upload async handlers.
+
+  Future<shelf.Response> _startUploadAsync(Uri uri) {
+    return repository.startAsyncUpload(_finishUploadAsyncUrl(uri))
+        .then((AsyncUploadInfo info) {
+      return _jsonResponse({
+        'url' : '${info.uri}',
+        'fields' : info.fields,
+      });
+    });
+  }
+
+  Future<shelf.Response> _finishUploadAsync(Uri uri) {
+    return repository.finishAsyncUpload(uri).then((_) {
+      return _jsonResponse({
+        'success' : {
+          'message' : 'Successfully uploaded package.',
+        },
+      });
+    }).catchError((error, stack) {
+      return _jsonResponse({
+        'error' : {
+          'message' : '$error.',
+        },
+      }, status: 400);
+    });
+  }
+
+
+  // Upload custom handlers.
+
+  Future<shelf.Response> _startUploadSimple(Uri url) {
+    _logger.info('Start simple upload.');
+    return _jsonResponse({
+      'url' : '${_uploadSimpleUrl(url)}',
+      'fields' : {},
+    });
+  }
+
+  Future<shelf.Response> _uploadSimple(
+      Uri uri, String contentType, Stream<List<int>> stream) {
+    _logger.info('Perform simple upload.');
+    if (contentType.startsWith('multipart/form-data')) {
+      var match = _boundaryRegExp.matchAsPrefix(contentType);
+      if (match != null) {
+        var boundary = match.group(1);
+        return stream
+            .transform(new MimeMultipartTransformer(boundary))
+            .first.then((MimeMultipart part) {
+          // TODO: Ensure that `part.headers['content-disposition']` is
+          // `form-data; name="file"; filename="package.tar.gz`
+          return repository.upload(part).then((_) {
+            return new shelf.Response.found(_finishUploadSimpleUrl(uri));
+          }).catchError((error, stack) {
+            // TODO: Do error checking and return error codes?
+            return new shelf.Response.found(
+                _finishUploadSimpleUrl(uri, error: error));
+          });
+        });
+      }
+    }
+    return
+        _badRequest('Upload must contain a multipart/form-data content type.');
+  }
+
+  Future<shelf.Response> _finishUploadSimple(Uri uri) {
+    var error = uri.queryParameters['error'];
+    _logger.info('Finish simple upload (error: $error).');
+    if (error != null) {
+      return _jsonResponse(
+          { 'error' : { 'message' : error } }, status: 400);
+    }
+    return _jsonResponse(
+        { 'success' : { 'message' : 'Successfully uploaded package.' } });
+  }
+
+
+  // Helper functions.
+
+  Future<shelf.Response> _badRequest(String message) {
+    return new Future.value(new shelf.Response(
+        400,
+        body: JSON.encode({ 'error' : message }),
+        headers: {'content-type': 'application/json'}));
+  }
+
+  Future<shelf.Response> _jsonResponse(Map json, {int status: 200}) {
+    return new Future.sync(() {
+      return new shelf.Response(status,
+          body: JSON.encode(json),
+          headers: {'content-type': 'application/json'});
+    });
+  }
+
+
+  // Metadata urls.
+
+  Uri _packageUrl(Uri url, String package) {
+    var encode = Uri.encodeComponent;
+    return url.resolve('/api/packages/${encode(package)}');
+  }
+
+
+  // Download urls.
+
+  Uri _downloadUrl(Uri url, String package, String version) {
+    var encode = Uri.encodeComponent;
+    return url.resolve(
+        '/packages/${encode(package)}/versions/${encode(version)}.tar.gz');
+  }
+
+
+  // Upload async urls.
+
+  Uri _startUploadAsyncUrl(Uri url) {
+    var encode = Uri.encodeComponent;
+    return url.resolve('/api/packages/versions/new');
+  }
+
+  Uri _finishUploadAsyncUrl(Uri url) {
+    var encode = Uri.encodeComponent;
+    return url.resolve('/api/packages/versions/newUploadFinish');
+  }
+
+
+  // Upload custom urls.
+
+  Uri _uploadSimpleUrl(Uri url) {
+    return url.resolve('/api/packages/versions/newUpload');
+  }
+
+  Uri _finishUploadSimpleUrl(Uri url, {String error}) {
+    var postfix = error == null ? '' : '?error=${Uri.encodeComponent(error)}';
+    return url.resolve('/api/packages/versions/newUploadFinish$postfix');
+  }
+}
---
+++ test/shelf_pubserver_test.dart
@@ -0,0 +1,371 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library pub_server.shelf_pubserver_test;
+
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:pub_server/repository.dart';
+import 'package:pub_server/shelf_pubserver.dart';
+import 'package:unittest/unittest.dart';
+
+class RepositoryMock implements PackageRepository {
+  final Function downloadFun;
+  final Function downloadUrlFun;
+  final Function finishAsyncUploadFun;
+  final Function lookupVersionFun;
+  final Function startAsyncUploadFun;
+  final Function uploadFun;
+  final Function versionsFun;
+
+  RepositoryMock(
+      {this.downloadFun, this.downloadUrlFun, this.finishAsyncUploadFun,
+       this.lookupVersionFun, this.startAsyncUploadFun, this.uploadFun,
+       this.versionsFun, this.supportsAsyncUpload: false,
+       this.supportsDownloadUrl: false, this.supportsUpload: false});
+
+  Future<Stream> download(String package, String version) async {
+    if (downloadFun != null) return downloadFun(package, version);
+    throw 'download';
+  }
+
+  Future<Uri> downloadUrl(String package, String version) async {
+    if (downloadUrlFun != null) return downloadUrlFun(package, version);
+    throw 'downloadUrl';
+  }
+
+  Future finishAsyncUpload(Uri uri) async {
+    if (finishAsyncUploadFun != null) return finishAsyncUploadFun(uri);
+    throw 'finishAsyncUpload';
+  }
+
+  Future<PackageVersion> lookupVersion(String package, String version) async {
+    if (lookupVersionFun != null) return lookupVersionFun(package, version);
+    throw 'lookupVersion';
+  }
+
+  Future<AsyncUploadInfo> startAsyncUpload(Uri redirectUrl) async {
+    if (startAsyncUploadFun != null) {
+      return startAsyncUploadFun(redirectUrl);
+    }
+    throw 'startAsyncUpload';
+  }
+
+  final bool supportsAsyncUpload;
+
+  final bool supportsDownloadUrl;
+
+  final bool supportsUpload;
+
+  Future upload(Stream<List<int>> data) {
+    if (uploadFun != null) return uploadFun(data);
+    throw 'upload';
+  }
+
+  Stream<PackageVersion> versions(String package) {
+    if (versionsFun != null) return versionsFun(package);
+    return new Stream.fromFuture(new Future.error('versions'));
+  }
+}
+
+Uri getUri(String path) => Uri.parse('http://www.example.com$path');
+
+shelf.Request getRequest(String path) {
+  var url = getUri(path);
+  return new shelf.Request('GET', url);
+}
+
+shelf.Request multipartRequest(Uri uri, List<int> bytes) {
+  var requestBytes = [];
+  String boundary = 'testboundary';
+
+  requestBytes.addAll(ASCII.encode('--$boundary\r\n'));
+  requestBytes.addAll(
+      ASCII.encode('Content-Type: application/octet-stream\r\n'));
+  requestBytes.addAll(ASCII.encode('Content-Length: ${bytes.length}\r\n'));
+  requestBytes.addAll(ASCII.encode('Content-Disposition: '
+                                   'form-data; name="file"; '
+                                   'filename="package.tar.gz"\r\n\r\n'));
+  requestBytes.addAll(bytes);
+  requestBytes.addAll(ASCII.encode('\r\n--$boundary--\r\n'));
+
+  var headers = {
+      'Content-Type' : 'multipart/form-data; boundary="$boundary"',
+      'Content-Length' : '${requestBytes.length}',
+  };
+
+  var body = new Stream.fromIterable([requestBytes]);
+  return new shelf.Request('POST', uri, headers: headers, body: body);
+}
+
+main() {
+  group('shelf_pubserver', () {
+    test('invalid endpoint', () async {
+      var mock = new RepositoryMock();
+      var server = new ShelfPubServer(mock);
+
+      testInvalidUrl(String path) async {
+        var request = getRequest(path);
+        var response = await server.requestHandler(request);
+        await response.read().drain();
+        expect(response.statusCode, equals(404));
+      }
+
+      await testInvalidUrl('/foobar');
+      await testInvalidUrl('/api');
+      await testInvalidUrl('/api/');
+      await testInvalidUrl('/api/packages/analyzer/0.1.0');
+    });
+
+    group('/api/packages/<package>', () {
+      test('does not exist', () async {
+        var mock =
+            new RepositoryMock(versionsFun: (_) => new Stream.fromIterable([]));
+        var server = new ShelfPubServer(mock);
+        var request = getRequest('/api/packages/analyzer');
+
+        var response = await server.requestHandler(request);
+        await response.read().drain();
+        expect(response.statusCode, equals(404));
+      });
+
+      test('success full retrieval of version', () async {
+        var mock = new RepositoryMock(versionsFun: (String package) {
+          // The pubspec is invalid, but that is irrelevant for this test.
+          var pubspec = JSON.encode({'foo': 1});
+          var analyzer = new PackageVersion('analyzer', '0.1.0', pubspec);
+          return new Stream.fromIterable([analyzer]);
+        });
+        var server = new ShelfPubServer(mock);
+        var request = getRequest('/api/packages/analyzer');
+        var response = await server.requestHandler(request);
+        var body = await response.readAsString();
+
+        var expectedVersionJson = {
+          'pubspec': {'foo': 1},
+          'version': '0.1.0',
+          'archive_url':
+              '${getUri('/packages/analyzer/versions/0.1.0.tar.gz')}',
+        };
+        var expectedJson = {
+          'name': 'analyzer',
+          'latest': expectedVersionJson,
+          'versions': [expectedVersionJson],
+        };
+
+        expect(response.mimeType, equals('application/json'));
+        expect(response.statusCode, equals(200));
+        expect(JSON.decode(body), equals(expectedJson));
+      });
+    });
+
+    group('/api/packages/<package>/versions/<version>', () {
+      test('does not exist', () async {
+        var mock = new RepositoryMock(lookupVersionFun: (_, __) => null);
+        var server = new ShelfPubServer(mock);
+        var request = getRequest('/api/packages/analyzer/versions/0.1.0');
+
+        var response = await server.requestHandler(request);
+        await response.read().drain();
+        expect(response.statusCode, equals(404));
+      });
+
+      test('success full retrieval of version', () async {
+        var mock = new RepositoryMock(
+            lookupVersionFun: (String package, String version) {
+          // The pubspec is invalid, but that is irrelevant for this test.
+          var pubspec = JSON.encode({'foo': 1});
+          return new PackageVersion(package, version, pubspec);
+        });
+        var server = new ShelfPubServer(mock);
+        var request = getRequest('/api/packages/analyzer/versions/0.1.0');
+        var response = await server.requestHandler(request);
+        var body = await response.readAsString();
+
+        var expectedJson = {
+          'pubspec': {'foo': 1},
+          'version': '0.1.0',
+          'archive_url':
+              '${getUri('/packages/analyzer/versions/0.1.0.tar.gz')}',
+        };
+
+        expect(response.mimeType, equals('application/json'));
+        expect(response.statusCode, equals(200));
+        expect(JSON.decode(body), equals(expectedJson));
+      });
+    });
+
+    group('/packages/<package>/versions/<version>.tar.gz', () {
+      group('download', () {
+        test('successfull redirect', () async {
+          var mock = new RepositoryMock(
+              downloadFun: (String package, String version) {
+            return new Stream.fromIterable([[1, 2, 3]]);
+          });
+          var server = new ShelfPubServer(mock);
+          var request = getRequest('/packages/analyzer/versions/0.1.0.tar.gz');
+          var response = await server.requestHandler(request);
+          var body = await response.read().fold([], (b, d) => b..addAll(d));
+
+          expect(response.statusCode, equals(200));
+          expect(body, equals([1, 2, 3]));
+        });
+      });
+
+      group('download url', () {
+        test('successfull redirect', () async {
+          var expectedUrl =
+              Uri.parse('https://blobs.com/analyzer-0.1.0.tar.gz');
+          var mock = new RepositoryMock(supportsDownloadUrl: true,
+              downloadUrlFun: (String package, String version) {
+            return expectedUrl;
+          });
+          var server = new ShelfPubServer(mock);
+          var request = getRequest('/packages/analyzer/versions/0.1.0.tar.gz');
+          var response = await server.requestHandler(request);
+          var body = await response.readAsString();
+
+          expect(response.statusCode, equals(303));
+          expect(response.headers['location'], equals('$expectedUrl'));
+        });
+      });
+    });
+
+    group('/api/packages/versions/new', () {
+      test('async successfull', () async {
+        var expectedUrl = Uri.parse('https://storage.googleapis.com');
+        var foobarUrl = Uri.parse('https://foobar.com/package/done');
+        var newUrl = getUri('/api/packages/versions/new');
+        var finishUrl = getUri('/api/packages/versions/newUploadFinish');
+        var mock = new RepositoryMock(
+            supportsUpload: true,
+            supportsAsyncUpload: true,
+            startAsyncUploadFun: (Uri redirectUri) {
+          expect(redirectUri, equals(finishUrl));
+          return new Future.value(
+              new AsyncUploadInfo(expectedUrl, {'a' : '$foobarUrl'}));
+        }, finishAsyncUploadFun: (Uri uri) {
+          expect('$uri', equals('$finishUrl'));
+        });
+        var server = new ShelfPubServer(mock);
+
+        // Start upload
+        var request =  new shelf.Request('GET', newUrl);
+        var response = await server.requestHandler(request);
+
+        expect(response.statusCode, equals(200));
+        expect(response.headers['content-type'], equals('application/json'));
+
+        var jsonBody = JSON.decode(await response.readAsString());
+        expect(jsonBody, equals({
+          'url' : '$expectedUrl',
+          'fields' : {
+            'a' : '$foobarUrl',
+          },
+        }));
+
+        // We would do now a multipart POST to `expectedUrl` which would
+        // redirect us back to the pub.dartlang.org app via `finishUrl`.
+
+        // Call the `finishUrl`.
+        request = new shelf.Request('GET', finishUrl);
+        response = await server.requestHandler(request);
+        jsonBody = JSON.decode(await response.readAsString());
+        expect(jsonBody, equals({
+          'success' : {
+            'message' : 'Successfully uploaded package.'
+          },
+        }));
+      });
+
+      test('sync successfull', () async {
+        var tarballBytes = const [1, 2, 3];
+        var newUrl = getUri('/api/packages/versions/new');
+        var uploadUrl = getUri('/api/packages/versions/newUpload');
+        var finishUrl = getUri('/api/packages/versions/newUploadFinish');
+        var mock = new RepositoryMock(
+            supportsUpload: true,
+            uploadFun: (Stream<List<int>> stream) {
+          return stream.fold([], (b, d) => b..addAll(d)).then((List<int> data) {
+            expect(data, equals(tarballBytes));
+          });
+        });
+        var server = new ShelfPubServer(mock);
+
+        // Start upload
+        var request =  new shelf.Request('GET', newUrl);
+        var response = await server.requestHandler(request);
+        expect(response.statusCode, equals(200));
+        expect(response.headers['content-type'], equals('application/json'));
+        var jsonBody = JSON.decode(await response.readAsString());
+        expect(jsonBody, equals({
+          'url' : '$uploadUrl',
+          'fields' : {},
+        }));
+
+        // Post data via a multipart request.
+        request =  multipartRequest(uploadUrl, tarballBytes);
+        response = await server.requestHandler(request);
+        await response.read();
+        expect(response.statusCode, equals(302));
+        expect(response.headers['location'], equals('$finishUrl'));
+
+        // Call the `finishUrl`.
+        request = new shelf.Request('GET', finishUrl);
+        response = await server.requestHandler(request);
+        jsonBody = JSON.decode(await response.readAsString());
+        expect(jsonBody, equals({
+          'success' : {
+            'message' : 'Successfully uploaded package.'
+          },
+        }));
+      });
+
+      test('sync failure', () async {
+        var tarballBytes = const [1, 2, 3];
+        var newUrl = getUri('/api/packages/versions/new');
+        var uploadUrl = getUri('/api/packages/versions/newUpload');
+        var finishUrl =
+            getUri('/api/packages/versions/newUploadFinish?error=abc');
+        var mock = new RepositoryMock(
+            supportsUpload: true,
+            uploadFun: (Stream<List<int>> stream) {
+          return new Future.error('abc');
+        });
+        var server = new ShelfPubServer(mock);
+
+        // Start upload - would happen here.
+
+        // Post data via a multipart request.
+        var request =  multipartRequest(uploadUrl, tarballBytes);
+        var response = await server.requestHandler(request);
+        await response.read();
+        expect(response.statusCode, equals(302));
+        expect(response.headers['location'], equals('$finishUrl'));
+
+        // Call the `finishUrl`.
+        request = new shelf.Request('GET', finishUrl);
+        response = await server.requestHandler(request);
+        var jsonBody = JSON.decode(await response.readAsString());
+        expect(jsonBody, equals({
+          'error' : {
+            'message' : 'abc'
+          },
+        }));
+      });
+
+      test('unsupported', () async {
+        var newUrl = getUri('/api/packages/versions/new');
+        var mock = new RepositoryMock();
+        var server = new ShelfPubServer(mock);
+        var request =  new shelf.Request('GET', newUrl);
+        var response = await server.requestHandler(request);
+
+        expect(response.statusCode, equals(404));
+      });
+    });
+  });
+}
