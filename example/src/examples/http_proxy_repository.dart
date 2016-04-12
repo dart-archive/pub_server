8272c48e05254c43f5de889da8be5c60677313a1
Keksinautin <keksinautin@gmail.com>
2016-04-12 18:46:11 +0300

    fix: the server breaks down if a package does not exists in the pub.dartlang.org

--- example/src/examples/http_proxy_repository.dart
+++ example/src/examples/http_proxy_repository.dart
@@ -29,16 +29,24 @@ class HttpProxyRepository extends PackageRepository {
           baseUrl.resolve('/api/packages/${Uri.encodeComponent(package)}');
 
       http.Response response = await client.get(versionUrl);
-      var json = JSON.decode(response.body);
-      var versions = json['versions'];
-      if (versions != null) {
-        return versions.map((Map item) {
-          var pubspec = item['pubspec'];
-          var pubspecString = JSON.encode(pubspec);
-          return new PackageVersion(
-              pubspec['name'], pubspec['version'], pubspecString);
-        }).toList();
+
+      try {
+        var json = JSON.decode(response.body);
+        var versions = json['versions'];
+        if (versions != null) {
+          return versions.map((Map item) {
+            var pubspec = item['pubspec'];
+            var pubspecString = JSON.encode(pubspec);
+            return new PackageVersion(
+                pubspec['name'], pubspec['version'], pubspecString);
+          }).toList();
+        }
+
+      } on FormatException catch (e) {
+        print(e.message);
+
       }
+
       return const [];
     }
 
